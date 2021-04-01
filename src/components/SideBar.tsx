5da78e7cff66c4361f780db3e5496680ca288ae8
Paulo Roberto <betinho5444@gmail.com>
2021-04-01 09:36:01 -0300

    refector: separating sidebar and content components from App.tsx

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,31 @@
-export function SideBar() {
+import {Button} from "./Button";
+
+import { GenreResponseProps } from '../App'
+
+type SideBarProps = {
+    genres: GenreResponseProps[];
+    handleClickButton: (id: number) => any;
+    selectedGenreId: number;
+}
+
+export function SideBar({ genres, handleClickButton, selectedGenreId }: SideBarProps) {
   // Complete aqui
+  return (
+      <nav className="sidebar">
+          <span>Watch<p>Me</p></span>
+
+          <div className="buttons-container">
+              {genres.map(genre => (
+                  <Button
+                      key={String(genre.id)}
+                      title={genre.title}
+                      iconName={genre.name}
+                      onClick={() => handleClickButton(genre.id)}
+                      selected={selectedGenreId === genre.id}
+                  />
+              ))}
+          </div>
+
+      </nav>
+  )
 }
